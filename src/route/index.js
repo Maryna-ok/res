1ed6e2b284fe039b5fbbe7bb259a3f3a4b9091dd
Maryna-ok <marina.proczko@gmail.com>
2023-08-04 00:51:39 +0200

    del program

--- src/route/index.js
+++ src/route/index.js
@@ -281,88 +281,7 @@ router.get('/work', function (req, res) {
 })
 // ================================================================
 
-<<<<<<< HEAD
-router.get('/program', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('program', {
-    layout: 'program',
-    program: {
-      excursion: {
-        name: 'Cultural Tour',
-        location: {
-          city: 'Paris',
-          country: 'France',
-        },
-        date: '2023-06-15',
-        program: [
-          {
-            name: 'Museum Visit',
-            type: 'Art',
-            duration: 3,
-            details: {
-              museum: {
-                name: 'The Louvre',
-                location: {
-                  address: 'Rue de Rivoli',
-                  city: 'Paris',
-                  country: 'France',
-                },
-                exhibits: [
-                  {
-                    name: 'Mona Lisa',
-                    artist: 'Leonardo da Vinci',
-                    description: 'Iconic portrait painting',
-                    audio_guide: true,
-                  },
-                  {
-                    name: 'Winged Victory of Samothrace',
-                    artist: null,
-                    description:
-                      'Ancient Greek statue of Nike, the goddess of victory',
-                    audio_guide: true,
-                  },
-                ],
-              },
-              guide: {
-                name: 'Francois',
-                language: 'French',
-                rating: 4.8,
-              },
-            },
-          },
-          {
-            name: 'Cultural Show',
-            type: 'Music and Dance',
-            duration: 2,
-            details: {
-              venue: {
-                name: 'Moulin Rouge',
-                location: {
-                  address: '82 Boulevard de Clichy',
-                  city: 'Paris',
-                  country: 'France',
-                },
-              },
-              performers: [
-                {
-                  name: 'Mireille Mathieu',
-                  type: 'Chanson singer',
-                },
-                {
-                  name: "Ballet de l'Opéra National de Paris",
-                  type: 'Classical ballet company',
-                },
-              ],
-              guide: {
-                name: 'Sophie',
-                language: 'English',
-                rating: 4.6,
-              },
-            },
-          },
-        ],
-      },
-=======
+
 //              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/person', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
@@ -458,15 +377,11 @@ router.get('/person', function (req, res) {
           ],
         },
       ],
->>>>>>> 3d5405b8f5e656458d2a6644de7d06250ceca812
+
     },
   })
 })
 
-<<<<<<< HEAD
 // ================================================================
-=======
-
->>>>>>> 3d5405b8f5e656458d2a6644de7d06250ceca812
 // Підключаємо роутер до бек-енду
 module.exports = router
